25bfc3cb4e4b59eb15fac759cfc3c02822133b74
vlad <Finogenov.V.A@yandex.ru>
2024-10-04 00:14:55 +0300

    fix: изменение настроек

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,4 @@
+import os
 
+media_dir = os.path.join("app", "presentations", "media")
+os.makedirs(media_dir, exist_ok=True)
--- app/main.py
+++ app/main.py
@@ -2,26 +2,28 @@ import os
 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
+from app.api import routes
 from app.core.logging_config import setup_logging
 from fastapi import FastAPI, status, Body, HTTPException, Request
 from fastapi.responses import HTMLResponse
 
-
+from app.core.config import media_dir
 
 setup_logging()
 
 
-media_dir = "media"
-os.makedirs(media_dir, exist_ok=True)
-
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="app/presentations/static"), name='static')
-# app.mount("/media", StaticFiles(directory=media_dir), name="media")
+app.mount("/media", StaticFiles(directory='app/presentations/media'), name="media")
 templates = Jinja2Templates(directory="app/presentations/templates")
 
 
 @app.get('/', response_class=HTMLResponse)
 async def get_webpage(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request, "message": "Contact Us"})
+    return templates.TemplateResponse("main.html", {"request": request, "message": "Contact Us"})
+
+
+app.include_router(routes.router)
 
 
